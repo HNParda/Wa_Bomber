da1e90421e3ab738f06a738678fa88830ffb4917
HNParda <memiliarda5@gmail.com>
2022-10-27 07:18:28 +0300

    .

--- app/src/main/java/com/korinistscheise/MainActivity.java
+++ app/src/main/java/com/korinistscheise/MainActivity.java
@@ -1,12 +1,22 @@
 package com.korinistscheise;
 
+import static com.korinistscheise.ardaBomber.getIDbyName;
+import static com.korinistscheise.ardaBomber.pref;
+import static com.korinistscheise.ardaBomber.save;
+import static java.lang.Integer.parseInt;
+
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,32 +28,91 @@ public class MainActivity extends AppCompatActivity {
 
     public void ardaBomber(View v) {
 
-        ardaBomber.init(this);
+        ardaBomber ardaBomber = new ardaBomber(this);
 
         AlertDialog.Builder Alert = new AlertDialog.Builder(this);
         View view = getLayoutInflater().inflate(R.layout.arda_bomber, null);
         Alert.setView(view);
 
+        AtomicInteger randomizer = new AtomicInteger(parseInt(pref.getString("bomber_randomizer", "0")));
+        String bomber_text = pref.getString("bomber_text", "BOOM!");
+        String bomber_count = pref.getString("bomber_count", "5");
 
-        RadioGroup rg = (RadioGroup) view.findViewById(R.id.radio);
+        String bak_rand = pref.getString("bomber_randomizer", "0");
+        String bak_text = pref.getString("bomber_text", "BOOM!");
+        String bak_count = pref.getString("bomber_count", "5");
+
+        RadioGroup rg = view.findViewById(getIDbyName(this, "radio"));
+        EditText count = view.findViewById(getIDbyName(this, "bomber_count"));
+        EditText text = view.findViewById(getIDbyName(this, "bomber_text"));
+
+        text.setText(bomber_text);
+        count.setText(bomber_count);
+        if (randomizer.get() == 1) rg.check(getIDbyName(this, "radio_1"));
+        if (randomizer.get() == 2) rg.check(getIDbyName(this, "radio_2"));
+
         rg.setOnCheckedChangeListener((group, checkedId) -> {
-            switch(checkedId){
-                case R.id.radio_1:
-                    // do operations specific to this selection
-                    break;
-                case R.id.radio_2:
-                    // do operations specific to this selection
-                    break;
-                case R.id.radio_3:
-                    // do operations specific to this selection
-                    break;
-            }
+
+            if (checkedId == getIDbyName(this, "radio_0")) randomizer.set(0);
+            else if (checkedId == getIDbyName(this, "radio_1")) randomizer.set(1);
+            else if (checkedId == getIDbyName(this, "radio_2")) randomizer.set(2);
+            save(String.valueOf(randomizer), "randomizer");
         });
 
+        text.addTextChangedListener(TextWatcher("text"));
+        count.addTextChangedListener(TextWatcher("count"));
 
         AlertDialog alertDialog = Alert.create();
         alertDialog.show();
         alertDialog.getWindow().setBackgroundDrawable(null);
+
+        alertDialog.setOnCancelListener(dialog -> {
+            save(bak_rand, "randomizer");
+            save(bak_count, "count");
+            save(bak_text, "text");
+        });
+
+        view.findViewById(getIDbyName(this, "arda_cancel")).setOnClickListener(v1 -> {
+            save(bak_rand, "randomizer");
+            save(bak_count, "count");
+            save(bak_text, "text");
+            alertDialog.dismiss();
+        });
+
+        view.findViewById(getIDbyName(this, "arda_save")).setOnClickListener(v1 -> {
+            save(String.valueOf(randomizer.get()), "randomizer");
+            save(count.getEditableText().toString(), "count");
+            save(text.getEditableText().toString(), "text");
+            alertDialog.dismiss();
+        });
+
+        view.findViewById(getIDbyName(this, "arda_send")).setOnClickListener(v1 -> {
+            ardaBomber.init(this, text.getEditableText().toString(), parseInt(count.getEditableText().toString()), randomizer.get());
+            ardaBomber.execute();
+            alertDialog.dismiss();
+        });
     }
 
+    public void sendMsg(String msg) {
+        Log.e("caused    testtest", msg);
+    }
+
+    TextWatcher TextWatcher(String string) {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                save(s.toString(), string);
+            }
+        };
+    }
 }
--- app/src/main/java/com/korinistscheise/ardaBomber.java
+++ app/src/main/java/com/korinistscheise/ardaBomber.java
@@ -3,36 +3,59 @@ package com.korinistscheise;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
-
-import java.util.Arrays;
+import android.util.Log;
 
 public class ardaBomber extends AsyncTask<Void, Void, String> {
 
     public static SharedPreferences pref;
-    String[] messages;
+    MainActivity ctx;
+    String msg;
     int randomizer;
+    int count;
 
-
-    public ardaBomber(String[] messages, int randomizer) {
-        this.messages = messages;
-        this.randomizer = randomizer;
-    }
-
-    public static void init(Context ctx) {
+    public ardaBomber(MainActivity ctx) {
         pref = ctx.getSharedPreferences("Arda", Context.MODE_PRIVATE);
     }
 
-    public static void saveMsg(String[] msg) {
-        pref.edit().putString("bomber", Arrays.toString(msg)).apply();
+    public static int getIDbyName(Context ctx, String resName) {
+        return ctx.getResources().getIdentifier(resName, "id", ctx.getPackageName());
     }
 
-    public static String loadMsg() {
-        return pref.getString("bomber", "[\"\"]");
+    public static void save(String save, String s) {
+        pref.edit().putString("bomber_" + s, save).apply();
+    }
+
+    public void init(MainActivity ctx, String msg, int count, int randomizer) {
+        this.ctx = ctx;
+        this.msg = msg;
+        this.randomizer = randomizer;
+        this.count = count;
     }
 
     @Override
     protected String doInBackground(Void... voids) {
+        if (randomizer == 0) for (int i = 0; i < count; i++)ctx.sendMsg(msg);
+
+        else if (randomizer == 1) {
+            String[] msgs = msg.split("\\$");
+            for (int i = 0; i < count; ) {
+                for (int e = 0; e < msgs.length && i < count; e++, i++) {
+                    ctx.sendMsg(msgs[e]);
+                    Log.e("caused fmkjjjjj", msgs[e]);
+                }
+
+            }
+        } else if (randomizer == 2) {
+            String[] msgs = msg.split("\\$");
+            for (int i = 0; i < count; i++) {
+                int rand = (int) (Math.random() * msgs.length);
+                Log.e("caused fmkjjjjj", String.valueOf(rand));
+                ctx.sendMsg(msgs[rand]);
+            }
+        }
+
         return null;
     }
+
 
 }
